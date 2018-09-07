9978e8a07740bfb466c459b6f267f0c23fb91e5f
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-07 16:48:04 +0300

    service interface'leri için base interface eklendi. Closes #12

---
+++ data/src/main/java/com/example/petclinic/service/CrudService.java
@@ -0,0 +1,16 @@
+package com.example.petclinic.service;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T owner);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
