92e3ad80c04d846e462863e70471f1516f4ccff4
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-07 16:33:37 +0300

    service interface'leri eklendi. Closes #4

---
+++ data/src/main/java/com/example/petclinic/service/VetService.java
@@ -0,0 +1,13 @@
+package com.example.petclinic.service;
+
+import com.example.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
