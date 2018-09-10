cc0b316ce9ff89394c0fffe1590ed29bfac4725e
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-10 10:46:43 +0300

    id servis içinde üretilecek, closes #24

--- web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Hasan");
         owner1.setLastName("Tekgül");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mahmut");
         owner2.setLastName("Tuncer");
 
@@ -37,7 +35,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Darth");
         vet1.setLastName("Vader");
 
