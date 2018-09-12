1db03963b3ef90af9f18d3f2adc9d6996b130edb
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-12 11:30:49 +0300

    service spring ifadesi eklendi

--- data/src/main/java/com/example/petclinic/service/map/PetTypeServiceMap.java
+++ data/src/main/java/com/example/petclinic/service/map/PetTypeServiceMap.java
@@ -2,6 +2,8 @@ package com.example.petclinic.service.map;
 
 import com.example.petclinic.model.PetType;
 import com.example.petclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
 }
