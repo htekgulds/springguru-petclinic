9c21f6a3c5700fc9b42328838f50050025e1d55a
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-12 11:19:51 +0300

    Owner modeline iletişim bilgisi ve Pet listesi eklendi, closes #29

--- data/src/main/java/com/example/petclinic/model/Owner.java
+++ data/src/main/java/com/example/petclinic/model/Owner.java
@@ -3,7 +3,14 @@ package com.example.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
+
+    private Set<Pet> pets;
 }
