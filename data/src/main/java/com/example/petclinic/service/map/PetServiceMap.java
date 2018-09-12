273a55d51c806265ff15f90d50bedaa6a93b2e62
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-12 11:28:14 +0300

    crud servis implement ettirilerek tekrar eden kod kaldırıldı

--- data/src/main/java/com/example/petclinic/service/map/PetServiceMap.java
+++ data/src/main/java/com/example/petclinic/service/map/PetServiceMap.java
@@ -9,28 +9,4 @@ import java.util.Set;
 
 @Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
-    @Override
-    public Set<Pet> findAll() {
-        return super.findAll();
-    }
-
-    @Override
-    public Pet findById(Long id) {
-        return super.findById(id);
-    }
-
-    @Override
-    public Pet save(Pet object) {
-        return super.save(object);
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        super.deleteById(id);
-    }
-
-    @Override
-    public void delete(Pet object) {
-        super.delete(object);
-    }
 }
