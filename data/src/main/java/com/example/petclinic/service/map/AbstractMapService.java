afe246b6ad7907d65c9d5a9ae6ef8008bb18fdb7
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-12 11:26:59 +0300

    pet type için map service impl eklendi, closes #30

--- data/src/main/java/com/example/petclinic/service/map/AbstractMapService.java
+++ data/src/main/java/com/example/petclinic/service/map/AbstractMapService.java
@@ -1,21 +1,22 @@
 package com.example.petclinic.service.map;
 
 import com.example.petclinic.model.BaseEntity;
+import com.example.petclinic.service.CrudService;
 
 import java.util.*;
 
-public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> implements CrudService<T, ID> {
     private Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll() {
+    public Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    T findById(ID id) {
+    public T findById(ID id) {
         return map.get(id);
     }
 
-    T save(T object) {
+    public T save(T object) {
         if (object != null) {
             if (object.getId() == null) {
                 object.setId(getNextId());
@@ -28,11 +29,11 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
         return object;
     }
 
-    void deleteById(ID id) {
+    public void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object) {
+    public void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
---
+++ data/src/main/java/com/example/petclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,7 @@
+package com.example.petclinic.service.map;
+
+import com.example.petclinic.model.PetType;
+import com.example.petclinic.service.PetTypeService;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+}
