16d9cf737133a7de64824885f597ab6ccc7f6217
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-13 10:15:19 +0300

    visit servisinde save için validation eklendi

--- data/src/main/java/com/example/petclinic/service/map/VisitMapService.java
+++ data/src/main/java/com/example/petclinic/service/map/VisitMapService.java
@@ -4,4 +4,15 @@ import com.example.petclinic.model.Visit;
 import com.example.petclinic.service.VisitService;
 
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null
+                || object.getPet().getOwner() == null
+                || object.getPet().getId() == null
+                || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(object);
+    }
 }
