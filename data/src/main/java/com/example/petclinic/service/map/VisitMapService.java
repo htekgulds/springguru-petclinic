5a0122bb03f962a9aca61843e7eca15cb579e6ff
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-13 10:12:26 +0300

    visit için map servisi eklendi, closes #45

---
+++ data/src/main/java/com/example/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,7 @@
+package com.example.petclinic.service.map;
+
+import com.example.petclinic.model.Visit;
+import com.example.petclinic.service.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+}
