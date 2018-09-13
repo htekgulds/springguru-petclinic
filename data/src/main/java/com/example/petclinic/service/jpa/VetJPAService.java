42be72af3173e668bf6f805f9a3e49240663abba
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2018-09-13 09:56:05 +0300

    base servisler düzeltildi

--- data/src/main/java/com/example/petclinic/service/jpa/VetJPAService.java
+++ data/src/main/java/com/example/petclinic/service/jpa/VetJPAService.java
@@ -3,6 +3,7 @@ package com.example.petclinic.service.jpa;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.repository.VetRepository;
 import com.example.petclinic.service.CrudService;
+import com.example.petclinic.service.VetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import java.util.Set;
 
 @Service
 @Profile("jpa")
-public class VetJPAService implements CrudService<Vet, Long> {
+public class VetJPAService implements VetService {
 
     private final VetRepository vetRepository;
 
